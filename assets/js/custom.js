31284c048ce9a7f5eb8c2efd87b17ec9804ed4db
Carlos Cámara <ccamara@pm.me>
2020-01-30 19:57:26 +0000

    Adds a readmore button.

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+function expandText() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readmore");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "+";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "-";
+    moreText.style.display = "inline";
+  }
+}
